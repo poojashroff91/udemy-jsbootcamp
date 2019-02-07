256200e0e43b55a2d1b8c4cd9471fb0c6474a938
poshroff <poshroff@paypal.com>
2019-02-07 13:31:44 -0800

    Adding source maps and environments

--- boilerplate/src/utilities.js
+++ boilerplate/src/utilities.js
@@ -1,8 +1,8 @@
-console.log('Utilities.js')
-
 export const add = (a,b) => a+b;
 
 export const name = 'Mike';
 
 const square = (x) => x * x;
 export default square;
+
+console.log('from my code');
